e6b4998afd0edac8244da49c46dab844bd01c11d
Helena Isåfjäll <heis18@student.bth.se>
2019-05-07 22:12:21 +0200

    Fixes here and there to make the unit tests work again.

---
+++ view/movie/movie-login.php
@@ -0,0 +1,26 @@
+
+<form method="post">
+    <fieldset class="search-movie">
+    <legend></legend>
+    <input type="hidden" name="route" value="search-title">
+
+    <h2>Log in to do changes in the movie-database</h2>
+
+    <p>
+        <label>Username:
+            <input type="text" name="user">
+        </label>
+    </p>
+    <p>
+        <label>Password:
+            <input type="password" name="password">
+        </label>
+    </p>
+
+    <p>
+        <input type="submit" name="login" value="Login">
+    </p>
+    </fieldset>
+
+        <a class="movie-link" href="index">Show all</a>
+</form>
